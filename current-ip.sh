d6ceafd8ac901f108609a441306b0158ca1cae86
Daniel Einars <contact@dle.dev>
2020-03-20 16:00:02 +0100

    fix: fail if `.env.current-ip` does not exist

--- current-ip.sh
+++ current-ip.sh
@@ -1,11 +1,10 @@
 #! /usr/bin/env bash
 
 
-IP=".env.current-ip"
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
-if [[ -f "$IP" ]] ; then
-    echo "removing previous $IP"
+if [[ -f ".env.current-ip" ]] ; then
     rm .env.current-ip
 fi
-
+printf '%s %s\n' "$(date): Updating ip file to contain $IP"
 echo `dig +short myip.opendns.com @resolver1.opendns.com` > .env.current-ip
--- update-dns-entries-if-ip-mismatch.sh
+++ update-dns-entries-if-ip-mismatch.sh
@@ -5,12 +5,11 @@ SETIP=`cat .env.current-ip`
 CURRENTIP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
 if [[ "$SETIP" != "$CURRENTIP" ]] ; then
-    echo "ips set in digital ocean currently do not match local ips."
-    echo "updating ips..."
-    for i in `doctl compute domain records list dle.dev | grep " A " | awk '{print $1}'`; do doctl compute domain records update dle.dev --record-id $i --record-data `dig +short myip.opendns.com @resolver1.opendns.com`; done
-    
+    printf '%s %s\n' "$(date): IPs do not match. Will update them."
+#    for i in `doctl compute domain records list dle.dev | grep " A " | awk '{print $1}'`; do doctl compute domain records update dle.dev --record-id $i --record-data `dig +short myip.opendns.com @resolver1.opendns.com`; done
+    bash ./current-ip.sh   
 else
-    echo "ips match. Doing nothing"
+    printf '%s %s\n' "$(date): IPs Match."
 fi
 
 
