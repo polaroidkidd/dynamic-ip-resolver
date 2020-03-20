0b4d2b80f9c95d701f755df64d1926db3754ac2e
Daniel Einars <contact@dle.dev>
2020-03-20 16:08:59 +0100

    fix: incorrent position for ip env file

--- update-dns-entries-if-ip-mismatch.sh
+++ update-dns-entries-if-ip-mismatch.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
 
-SETIP=`cat .env.current-ip`
+SETIP=`cat /home/dle/dynamic-ip-resolver/.env.current-ip`
 CURRENTIP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
 if [[ "$SETIP" != "$CURRENTIP" ]] ; then
     printf '%s %s\n' "$(date): IPs do not match. Will update them."
-#    for i in `doctl compute domain records list dle.dev | grep " A " | awk '{print $1}'`; do doctl compute domain records update dle.dev --record-id $i --record-data `dig +short myip.opendns.com @resolver1.opendns.com`; done
-    bash ./current-ip.sh   
+    for i in `doctl compute domain records list dle.dev | grep " A " | awk '{print $1}'`; do doctl compute domain records update dle.dev --record-id $i --record-data `dig +short myip.opendns.com @resolver1.opendns.com`; done
+    bash /home/dle/dynamic-ip-resolver/.env.current-ip/current-ip.sh   
 else
     printf '%s %s\n' "$(date): IPs Match."
 fi
